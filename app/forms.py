fd8d73bbd91d2993e2ef0eeaa4996d6b4e54b629
devsahu8 <devsahu7180@gmail.com>
2026-02-20 23:52:01 +0530

    simple index template added

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,IntegerField
+from wtforms.validators import DataRequired
+
+class IndexForm(FlaskForm):
+    amount=IntegerField("Enter the complete amount of savings you have.",validators=[DataRequired()])
+    submit=SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from app.forms import IndexForm
+from flask import redirect,render_template,url_for
 
-@app.route("/")
-@app.route("/index")
+@app.route("/",methods=["GET","POST"])
+@app.route("/index",methods=["GET","POST"])
 def index():
-    return "Hello World!"
+    form=IndexForm()
+    if form.validate_on_submit():
+        return redirect(url_for("info"))
+    return render_template("index.html",form=form)
